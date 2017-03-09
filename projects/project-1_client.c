269ef55feb868e42a47a2a26817adb954eb3c121
dksteele <dksteele@captechu.edu>
2017-03-08 18:47:10 -0800

    Commitn

--- projects/project-1_client.c
+++ projects/project-1_client.c
@@ -1,24 +1,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
 
+
+#define BUFFER_SIZE 1024
 int main(int argc, char* argv[]){
 	
 	int s;
 	struct sockaddr_in addr;
 	
-	if(argc != 2){
-		printf("Usage: %s <server_port>\n", argv[0]);
+	if(argc != 3){
+		printf("Usage: %s <server_ip_address> <server_port>\n", argv[0]);
 		return 0;
 	}
 	
 	memset(&addr, '0', sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(atoi(argv[1]));
+	addr.sin_port = htons(atoi(argv[2]));
 	
 	if(inet_pton(AF_INET, argv[1], &addr.sin_addr) <= 0){
 		printf("Error Parsing Ip Address: %s\n", argv[1]);
@@ -37,22 +40,35 @@ int main(int argc, char* argv[]){
 	
 	printf("CONNECTED TO %s:%s\n", argv[1], argv[2]);
 	
+	
+	char buf[BUFFER_SIZE];
 	while(1){
 	
-		char *in, *out;
-		printf("Enter Echo Text: ");
+		char *out;
+		printf("Enter File Name: ");
 		scanf("%m[^\n]%*c", &out);
 		
 		send(s, out, strlen(out),0);
 		
-		in = (char*) malloc(sizeof(char) * strlen(out));
-		recv(s, in, strlen(out), 0);
+		int last_line = 0;
 		
-		printf("%s\n", in);
+		while(!last_line){
+			memset(&buf, '\0', sizeof(buf));
+			
+			if(!recv(s, buf, BUFFER_SIZE, 0)){
+				printf("Connection Terminated\n");
+				return 1;
+			}
+			
+			if(buf[strlen(buf) - 1] == EOF){
+				buf[strlen(buf) - 1] = '\0'; //Replace EOF with null
+				last_line = 1;
+			}
+			printf("%s", buf);
+		}
 	}
 	
 	close(s);
 	
-	return 1;
+	return 0;
 }
-	
---
+++ projects/project-1_server.c
@@ -0,0 +1,93 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char* argv[]){
+	
+	int s;
+	struct sockaddr_in addr;
+	char cwd[BUFFER_SIZE];
+	
+	getcwd(cwd, BUFFER_SIZE);
+	
+	if(argc != 2){
+		printf("Usage: %s <server_ip_address> <server_port>\n", argv[0]);
+		return 0;
+	}
+	
+	memset(&addr, '0', sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(atoi(argv[1]));
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if((s = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		printf("Error Creating Socket\n");
+		return 0;
+	}
+	
+	int opt_val = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
+    
+	if(bind(s, (struct sockaddr *) &addr, sizeof(addr)) < 0){
+		printf("Error Binding Socket\n");
+		return 0;
+	}
+	
+	if(listen(s, 128) < 0){
+		printf("Error Listenting\n");
+		return 0;
+	}
+	
+	while(1){
+		int c;
+        if ((c = accept(s, (struct sockaddr *) NULL, NULL)) < 0){ 
+			printf("Error Creating Connection\n");
+		}
+		if(fork() == 0){
+			close(s);
+			
+			char buf[BUFFER_SIZE];
+			while (1){				
+				
+				memset(&buf, '\0', sizeof(buf));
+				if(!recv(c, buf, BUFFER_SIZE, 0))
+					break;
+				
+				char path[strlen(buf) + strlen(cwd) + 2];
+				memcpy(path, cwd, strlen(cwd));
+				path[strlen(cwd)] = '/';
+				memcpy(&path[strlen(cwd) + 1], buf, strlen(buf));
+				path[strlen(buf) + strlen(cwd) + 1] = '\0';
+				
+				printf("%s\n", path);
+			
+				FILE *file = fopen(path, "r");
+				if(file == NULL){
+					send(c, "ERROR: File Not Found\n", 22, 0); 
+				}else{	
+					char line_buf[1024];
+					while(fgets(line_buf, 1024, file) != NULL){
+						 send(c, line_buf, strlen(line_buf), 0);
+					 }
+					 fclose(file);
+				}
+				
+				char eof = EOF;
+				send(c, &eof, 1, 0);
+			}
+			close(c);
+			exit(0);
+		}
+		close(c);
+	}
+	
+	return 0;
+}
+	
