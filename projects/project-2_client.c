db7df6aadd2b4258a72a2db4cf36cf7bb45d79da
dksteele <dksteele@captechu.edu>
2017-04-11 12:04:20 -0700

    Project 1 Working But Blocking Issues Still Need To Finish Service Portion

--- projects/project-2_client.c
+++ projects/project-2_client.c
@@ -1,27 +1,53 @@
-#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <arpa/inet.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/socket.h>
 #include <time.h>
-
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
+
+char *last_msg;
+int sock;
+
+void recieve_signal_handler(int sig_type)
+{
+	struct sockaddr_in cli_addr;
+	char recv_buf[BUFFER_SIZE];
+	int num_chars;
+	socklen_t slen = sizeof(cli_addr);
+	
+	if(!(num_chars = recvfrom(sock, recv_buf, BUFFER_SIZE, 0, (struct sockaddr*) &cli_addr, &slen))){
+		perror("Recieving");
+	}
+	recv_buf[num_chars] = '\0';
+	printf("%s\n", recv_buf);
+}
+
 int main(int argc, char* argv[]){
 	
-	int recv_sock, send_sock;
-	struct sockaddr_in serv_addr;
+	struct sockaddr_in serv_addr, broad_addr;
 	struct hostent *host_ip;
-	char recv_buf[BUFFER_SIZE];
-	
+
+	last_msg = "\0";
+
 	if(argc != 3){
 		printf("Usage: %s <server_hostname> <server_port>\n", argv[0]);
 		return 0;
 	}
+	
+	signal(SIGIO, recieve_signal_handler);
+	
+	memset(&broad_addr, '0', sizeof(broad_addr));
+	broad_addr.sin_family = AF_INET;
+	broad_addr.sin_port = htons(1900);
+	broad_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
 	memset(&serv_addr, '0', sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
@@ -34,56 +60,57 @@ int main(int argc, char* argv[]){
 	}
 	
 	serv_addr.sin_addr = *(struct in_addr *) host_ip->h_addr;
-	
-	if((send_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
-		perror("Creating Sending Socket\n");
-		return 0;
-	}
-	
-	if((recv_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+
+	if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
 		perror("Creating Recieving Socket\n");
 		return 0;
 	}
 	
-	if(fork() == 0){
-		close(recv_sock);
-		
-		while(1){
-			
-			char *in;
-			scanf("%m[^\n]%*c", &in);
-			
-			time_t tm = time(NULL);
-			char *str_time = ctime(&tm);
-			str_time[strlen(str_time) - 1] = '\0'; //Remove new line and null terminate string
-			
-			char *user = getenv("USER");
-			
-			
-			char *out = malloc(strlen(in) + strlen(str_time) + strlen(user) + 7);
-			strcpy(out, str_time);
-			strcat(out, " [");
-			strcat(out, user);
-			strcat(out, "] : ");
-			strcat(out, in);
-			strcat(out, "\0"); //Null terminate string
-			
-			if(!sendto(send_sock, out, strlen(out), 0, (struct sockaddr*) &serv_addr, sizeof(serv_addr)))
-				perror("Sending");
-		}
-		close(send_sock);
-	}else{
-		close(send_sock);
-		
-		while(1){
-			if(recv(recv_sock, recv_buf, BUFFER_SIZE, 0)){
-				recv_buf[strlen(recv_buf) - 1] = '\0';
-				printf("%s", recv_buf);
-			}
-		}
-		
-		close(recv_sock);
+	if(bind(sock, (struct sockaddr* ) &broad_addr, sizeof(broad_addr))){
+		perror("Binding Socket\n");
+		return 0;
 	}
+	
+	int enable_broadcast = 1;
+	setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &enable_broadcast, sizeof(enable_broadcast));
+	
+	if(fcntl(sock, F_SETOWN, getpid())){
+		perror("Setting Socket Owner");
+		return 0;
+	}
+	
+	if(fcntl(sock, F_SETFL, O_ASYNC)){
+		perror("Setting async and nonblocking");
+		return 0;
+	}
+	
+	while(1){
+		
+		char in[BUFFER_SIZE];
+		fgets(in, BUFFER_SIZE, stdin);
+		
+		time_t tm = time(NULL);
+		char *str_time = ctime(&tm);
+		str_time[strlen(str_time) - 1] = '\0'; //Remove new line and null terminate string
+		
+		char *user = getenv("USER");
+		
+		
+		char *out = malloc(strlen(in) + strlen(str_time) + strlen(user) + 7);
+		strcpy(out, str_time);
+		strcat(out, " [");
+		strcat(out, user);
+		strcat(out, "] : ");
+		strcat(out, in);
+		strcat(out, "\0"); //Null terminate string
+		
+		last_msg = malloc(strlen(out));
+		strcpy(last_msg, out);
+		
+		if(!sendto(sock, out, strlen(out), 0, (struct sockaddr*) &serv_addr, sizeof(serv_addr)))
+			perror("Sending");
+	}
+	close(sock);
 	
 	return 0;
 }
